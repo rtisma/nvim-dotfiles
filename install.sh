752d43fa5ae7315ac069ad8edaf4f4b029677c26
Robert Tisma <rtisma@gmail.com>
2019-04-18 15:21:49 -0400

    updated

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+if [ -f ~/.config/nvim/init.vim ];then
+	echo "The file `~/.config/nvim/init.vim` already exists. move it and re-run"
+	exit 1
+else
+	curl -fLo ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
